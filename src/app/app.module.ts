e286f0d261c93acf06481b08be246978bf9eb027
Sini Ann <macbook@Macbooks-MacBook-Pro.local>
2018-05-04 15:18:07 +0200

    angular project first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';  //added for forms
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // added for animations
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //added for forms
+    BrowserAnimationsModule, AppRoutingModule // added for animations
   ],
   providers: [],
   bootstrap: [AppComponent]
